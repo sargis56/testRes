cae6af6672abdb68f27bb6c2b31e43526854c3b8
Sargis <sargis@live.ca>
2017-09-29 19:34:23 -0400

    Uploading Lab 3

---
+++ Lab Stuff/COMP305-F2017_Lab-3/Assets/Scripts/BottleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleController : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    private Rigidbody2D rigBody;
+
+    // Use this for initialization
+    void Start () {
+        rigBody = this.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+        float horizMove = Input.GetAxis("Horizontal");
+        float vertMove = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizMove, vertMove, 0);
+        rigBody.velocity = movement * speed;
+    }
+}
---
+++ Lab Stuff/COMP305-F2017_Lab-3/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject camera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((player.GetComponent<Rigidbody2D>().position.x >= 9.46) && (player.GetComponent<Rigidbody2D>().position.x < 30.36485))
+        {
+            camera.GetComponent<Rigidbody2D>().transform.position = new Vector3(20.64f, 0, -10f);
+            camera.GetComponent<Camera>().orthographicSize = 5;
+        }
+        else if (player.GetComponent<Rigidbody2D>().position.x >= 30.36485)
+        {
+            camera.GetComponent<Rigidbody2D>().transform.position = new Vector3(player.GetComponent<Rigidbody2D>().transform.position.x, player.GetComponent<Rigidbody2D>().transform.position.y, -10f);
+            camera.GetComponent<Camera>().orthographicSize = 50 - (player.GetComponent<Rigidbody2D>().transform.position.x);
+        }
+        else
+        {
+            camera.GetComponent<Rigidbody2D>().transform.position = new Vector3(player.GetComponent<Rigidbody2D>().transform.position.x, player.GetComponent<Rigidbody2D>().transform.position.y, -10f);
+            camera.GetComponent<Camera>().orthographicSize = 5;
+        }
+    }
+}
